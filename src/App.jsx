4ee4d0cd142f22d0af1f79a084d889704d2c0adc
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-11-22 22:17:07 +0530

    feat: added basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentInput, setCurrentInput] = useState('');
+  const [items, setItems] = useState([]);
+
+  const handleInput = (e) => {
+    setCurrentInput(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Item added!');
+    const newItem = {key: items.length+1, value: currentInput, checked: false}
+    const newToDoItems = [...items, newItem];
+    setItems(newToDoItems);
+    setCurrentInput('');
+  }
+
+  const handleCheckboxChange = (index) => {
+    setItems((prevItems) =>
+      prevItems.map((item, i) =>
+        i === index ? { ...item, checked: !item.checked } : item
+      )
+    );
+  };
 
   return (
-    <>
+    <div> 
+      <form onSubmit={handleSubmit}>
+      <div className="input-group mb-3">
+        <span className="input-group-text" id="basic-addon1">To-Do Item</span>
+        <input value={currentInput} onChange = {handleInput} type="text" className="form-control" placeholder="Add your to-do item" aria-label="Add your to-do item" aria-describedby="basic-addon2" />
+        <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Add item</button>
+        </div>
+      </form>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {items.map((item, index) => {
+            return(
+            <li key={index+1}>{item.value}   
+            <input className="form-check-input mt-0" checked={item.checked} onChange={() => handleCheckboxChange(index)} type="checkbox" value="" aria-label="Checkbox to check off completed items"></input>   
+            </li>
+            )
+          })}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
