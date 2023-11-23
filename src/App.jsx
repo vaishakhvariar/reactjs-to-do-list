ac41dfeede45eedbc052bf6e287d1799abcad84c
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-11-23 20:33:48 +0530

    feat: added edit and delete options. added localStorage to persist data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [currentInput, setCurrentInput] = useState('');
   const [items, setItems] = useState([]);
+  const [currentEditableIndex, setCurrentEditableIndex] = useState(null);
+  const [updatedInput, setUpdatedInput] = useState('');
+
+  useEffect(()=>{
+    const localItems = JSON.parse(localStorage.getItem('items')); 
+    if (localItems) {
+    setItems(localItems);
+    }
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem('items',JSON.stringify(items));
+  }, [items])
 
   const handleInput = (e) => {
     setCurrentInput(e.target.value);
@@ -11,12 +24,31 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setCurrentEditableIndex(null);
+    if(currentInput==='') {return;}
     console.log('Item added!');
     const newItem = {key: items.length+1, value: currentInput, checked: false}
     const newToDoItems = [...items, newItem];
     setItems(newToDoItems);
     setCurrentInput('');
   }
+
+  const handleUpdate = (index) => {
+    if(updatedInput===''){
+      return;
+    }
+    setItems((prevItems)=> {
+    const updatedItem = {key: index, value: updatedInput, checked: false}
+    console.log(updatedItem);
+    const updatedToDoItems = [...prevItems];
+    updatedToDoItems[index] = updatedItem;
+    setCurrentEditableIndex(null);
+    setUpdatedInput('');
+    return updatedToDoItems;
+  }
+    );
+}
+
 
   const handleCheckboxChange = (index) => {
     setItems((prevItems) =>
@@ -25,6 +57,20 @@ function App() {
       )
     );
   };
+
+  const handleEdit = (index) => {
+    setCurrentEditableIndex(index);   
+  }
+
+  const handleUpdatedInput = (e) => {
+    setUpdatedInput(e.target.value)
+  }
+
+  const handleDelete = (index) => {
+    setItems((prevItems) => 
+    prevItems.filter((_,i)=>i!==index)
+    );
+  }
 
   return (
     <div> 
@@ -39,8 +85,21 @@ function App() {
         <ul>
           {items.map((item, index) => {
             return(
-            <li key={index+1}>{item.value}   
-            <input className="form-check-input mt-0" checked={item.checked} onChange={() => handleCheckboxChange(index)} type="checkbox" value="" aria-label="Checkbox to check off completed items"></input>   
+            <li key={index+1}>
+            <div className="d-flex justify-content-between">
+            {currentEditableIndex===index?
+            <input value={updatedInput} onChange = {handleUpdatedInput} type="text" className="form-control" placeholder="Edit this item" aria-label="Edit this item" aria-describedby="basic-addon2" />
+            :<span>{item.value}</span>
+            }
+            <div>  
+            <input className="form-check-input mt-0" checked={item.checked} onChange={() => handleCheckboxChange(index)} type="checkbox" value="" aria-label="Checkbox to check off completed items"></input> 
+            {currentEditableIndex!==index?
+            <button className="btn btn-outline-secondary" onClick={()=>handleEdit(index)} type="button" id="button-addon3">Edit item</button>
+            :<button className="btn btn-outline-secondary" onClick={()=>handleUpdate(index)} type="submit" id="button-addon3">Update item</button> 
+            }
+            <button className="btn btn-outline-secondary" onClick={()=> handleDelete(index)} type="button" id="button-addon4">Delete item</button>
+            </div>
+            </div>
             </li>
             )
           })}
